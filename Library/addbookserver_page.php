3d5c5fb9c0b7272e8f70d495ea4f28a3f6c5fb1b
sidduamand2004 <amandsrisiddhartha@gmail.com>
2024-01-14 01:36:23 +0530

    Digital-Library-php

---
+++ Library/addbookserver_page.php
@@ -0,0 +1,23 @@
+<?php
+//addserver_page.php
+include("data_class.php");
+
+$bookname=$_POST['bookname'];
+$bookdetail=$_POST['bookdetail'];
+$bookaudor=$_POST['bookaudor'];
+$bookpub=$_POST['bookpub'];
+$branch=$_POST['branch'];
+$bookprice=$_POST['bookprice'];
+
+if (move_uploaded_file($_FILES["bookphoto"]["tmp_name"],"uploads/" . $_FILES["bookphoto"]["name"])) {
+
+    $bookpic=$_FILES["bookphoto"]["name"];
+
+$obj=new data();
+$obj->setconnection();
+$obj->addbook($bookpic,$bookname,$bookdetail,$bookaudor,$bookpub,$branch,$bookprice);
+  } 
+  else
+   {
+     echo "File not uploaded";
+  }
